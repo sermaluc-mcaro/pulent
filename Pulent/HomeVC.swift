81256b335ce256d625e212f853fd29d65b6bed9c
Mauricio Caro <mauricio.caro@sermaluc.cl>
2019-11-08 21:35:54 -0300

    08-11-19 21:34 Commit inicial, diseño de interface con controlador HomeVC

---
+++ Pulent/HomeVC.swift
@@ -0,0 +1,38 @@
+//
+//  HomeVC.swift
+//  Pulent
+//
+//  Created by Mauricio Caro on 08-11-19.
+//  Copyright © 2019 Mauricio Caro. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+//MARK:- Delegate UITableView
+
+extension HomeVC:UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
---
+++ Pulent/NetworkService.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkService.swift
+//  Pulent
+//
+//  Created by Mauricio Caro on 08-11-19.
+//  Copyright © 2019 Mauricio Caro. All rights reserved.
+//
+
+import UIKit
+
+class NetworkService: NSObject {
+
+    
+    static let _shared  = NetworkService()
+    static var shared:NetworkService{
+        return _shared
+    }
+    
+    
+    
+    
+}
